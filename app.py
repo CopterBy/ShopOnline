d24d908a4d2f21f858c83372900ed50ab9cb88d3
Max <info@specdron.by>
2022-12-11 11:31:25 +0300

    Урок 9: Added a new data base & table

--- app.py
+++ app.py
@@ -1,15 +1,41 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-db = SQLAlchemy()
+# Defining the Engine
+engine = db.create_engine('sqlite:///shop.db', echo=True)
 
-class Item(db.Model):
-    id = db.Colum(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    price = db.Colum(db.Integer, nullable=False)
-    isActive = db.Colum(db.Integer, nullable=False)
+# Create the Metadata Object
+metadata_obj = db.MetaData()
+
+# Define the profile table
+
+# database name
+profile = db.Table(
+    'profile',
+    metadata_obj,
+    db.Column('id', db.Integer, primary_key=True),
+    db.Column('title', db.String(100), nullable=False),
+    db.Column('price', db.Integer, nullable=False),
+    db.Column('isActive', db.Boolean, default=True),
+)
+
+# Create the profile table
+metadata_obj.create_all(engine)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+# db = SQLAlchemy(app)
+#
+# class Item(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(100), nullable=False)
+#     price = db.Column(db.Integer, nullable=False)
+#     isActive = db.Column(db.Boolean, default=True)
+#     # text = db.Column(db.Text, nullable=False)
+#
+#     def __repr__(self):
+#         return '<Article %r' % self.id
 
 
 @app.route('/')
