7c7f9b3b7dceffecd7f430addcc461776f294e85
Max <info@specdron.by>
2022-12-11 12:31:22 +0300

    Урок 15: Processing the click of the Add button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as db
 
@@ -29,29 +29,43 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
 db = SQLAlchemy(app)
 
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
+
     # text = db.Column(db.Text, nullable=False)
 
-    def __repr__(self):
-        return '<Article %r' % self.id
+    # def __repr__(self):
+    #     return '<Item %r' % self.id
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/create', methods=['POST'], ['GET'])
+
+@app.route('/create', methods=['POST', 'GET'])
 def create():
-    if request.method == 'post':
-        pass
+    if request.method == 'POST':
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Ошибка добавления товара'
     else:
         return render_template('create.html')
 
