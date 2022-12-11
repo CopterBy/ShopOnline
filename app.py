3e4d75be769e2355a51f491d14e2fb16bc641c0f
Max <info@specdron.by>
2022-12-11 11:44:03 +0300

    Урок 11: Added create.html

--- app.py
+++ app.py
@@ -3,39 +3,41 @@ from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as db
 
 app = Flask(__name__)
+
+# # Create data base
 # Defining the Engine
-engine = db.create_engine('sqlite:///shop.db', echo=True)
-
-# Create the Metadata Object
-metadata_obj = db.MetaData()
-
-# Define the profile table
-
-# database name
-profile = db.Table(
-    'profile',
-    metadata_obj,
-    db.Column('id', db.Integer, primary_key=True),
-    db.Column('title', db.String(100), nullable=False),
-    db.Column('price', db.Integer, nullable=False),
-    db.Column('isActive', db.Boolean, default=True),
-)
-
-# Create the profile table
-metadata_obj.create_all(engine)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-# db = SQLAlchemy(app)
+# engine = db.create_engine('sqlite:///shop.db', echo=True)
 #
-# class Item(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(100), nullable=False)
-#     price = db.Column(db.Integer, nullable=False)
-#     isActive = db.Column(db.Boolean, default=True)
-#     # text = db.Column(db.Text, nullable=False)
+# # Create the Metadata Object
+# metadata_obj = db.MetaData()
 #
-#     def __repr__(self):
-#         return '<Article %r' % self.id
+# # Define the profile table
+#
+# # database name
+# profile = db.Table(
+#     'profile',
+#     metadata_obj,
+#     db.Column('id', db.Integer, primary_key=True),
+#     db.Column('title', db.String(100), nullable=False),
+#     db.Column('price', db.Integer, nullable=False),
+#     db.Column('isActive', db.Boolean, default=True),
+# )
+#
+# # Create the profile table
+# metadata_obj.create_all(engine)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+    # text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<Article %r' % self.id
 
 
 @app.route('/')
@@ -45,6 +47,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
